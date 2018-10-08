e346d8e7aa53ffd0e92c8cae22fc2775e54df132
TKCSeow <33025216+TKCSeow@users.noreply.github.com>
2018-10-08 15:52:52 +0100

    Initial commit, Unity project created

---
+++ Robot Run Prototype/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Adapted from Unity Tutorials
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, 0.0f, z);
+
+        rigidbody.AddForce(movement * speed);
+
+
+    }
+}
