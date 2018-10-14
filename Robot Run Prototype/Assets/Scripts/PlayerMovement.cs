f0487672211a22c7f0e9ca515bf901ab8e6c18c2
TKCSeow <33025216+TKCSeow@users.noreply.github.com>
2018-10-14 20:50:57 +0100

    Coded Movement, Camera, TileManager, ScoreManager

--- Robot Run Prototype/Assets/Scripts/PlayerMovement.cs
+++ Robot Run Prototype/Assets/Scripts/PlayerMovement.cs
@@ -6,22 +6,57 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float speed;
-    private Rigidbody rigidbody;
+    public float speed = 10.0f;
+    private CharacterController controller;
+    private Vector3 moveVector;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 12.0f;
+
+    private float animationDuration = 3.0f;
+
+    //private Animator anim;
 
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+        //anim = GetComponent<Animator>();
     }
     void Update()
     {
-        var x = Input.GetAxis("Horizontal");
-        var z = Input.GetAxis("Vertical");
+        // During Opening Animation
+        if(Time.time < animationDuration)
+        {
+            controller.Move(Vector3.forward * speed * Time.deltaTime);
 
-        Vector3 movement = new Vector3(x, 0.0f, z);
+            return;
+        }
 
-        rigidbody.AddForce(movement * speed);
+        moveVector = Vector3.zero;
 
+        moveVector.x = Input.GetAxisRaw("Horizontal") * (speed / 2);
+
+        // Gravity/Falling
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0.1f;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        moveVector.y = verticalVelocity;
+
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
+        //anim.Play("Locomotion");
 
     }
+
+    public void SetSpeed(float modifier)
+    {
+        speed = 5 + modifier;
+    }
+
 }
---
+++ Robot Run Prototype/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    // Use this for initialization
+    private GameObject player;
+
+    private float score = 0.0f;
+    public Text scoreText;
+
+    private int difficultyLevel = 1;
+    private int maxDifficultyLevel = 10;
+    private int scoreToNextLevel = 10;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+
+        if (score >= scoreToNextLevel)
+        {
+            LevelUp();
+        }
+
+        score += Time.deltaTime;
+        scoreText.text = ((int)score).ToString();
+
+    }
+
+     private void LevelUp()
+    {
+        if (difficultyLevel == maxDifficultyLevel)
+        {
+            return;
+        }
+
+        scoreToNextLevel *= 2;
+        difficultyLevel++;
+       
+
+        player.GetComponent<PlayerMovement>().SetSpeed(difficultyLevel * 5);
+    }
+
+}
